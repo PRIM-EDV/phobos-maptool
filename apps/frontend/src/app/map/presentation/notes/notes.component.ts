40d2599bddc6e5759fa1e17939a5a3dfbb657fcf
Lucas Mösch <lucasmoesch@live.de>
2025-08-04 20:40:06 +0000

    Added notes component boilerplate for map

---
+++ apps/frontend/src/app/map/presentation/notes/notes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "notes",
+  standalone: true,
+  templateUrl: "./notes.component.html",
+  styleUrls: ["./notes.component.scss"],
+  imports: [],
+})
+export class NotesComponent {}
