e5e15b0c9cf9c5f0f8b494667729282ebb28ce35
Lucas Mösch <lucasmoesch@live.de>
2025-08-16 20:42:38 +0200

    Added websocket reconnect

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -8,55 +8,60 @@ import { MaptoolGateway } from './infrastructure/maptool.gateway';
 import { OverlayComponent } from './overlay/overlay.component';
 
 declare global {
-    interface Window {
-      __env: {
-        lsxServerHostname: string,
-        lsxServerPort: string,
-        trxServerHostname: string,
-        trxServerPort: string
-      }
+  interface Window {
+    __env: {
+      lsxServerHostname: string,
+      lsxServerPort: string,
+      MAPTOOL_SERVER_HOSTNAME: string,
+      MAPTOOL_SERVER_PORT: string
     }
   }
+}
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [
-      ContextMenuModule,
-      DialogComponent,
-      RouterOutlet,
-      // OverlayComponent
-    ],
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    ContextMenuModule,
+    DialogComponent,
+    RouterOutlet,
+    // OverlayComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
-    autoGatewayConnection = effect(() => {
-      if (this.tokenService && !this.maptoolGateway.isConnected()) {
-          this.connectToMaptoolGateway().catch(error => {
-            console.error('Failed to connect to Maptool Gateway:', error);
-          });
-      }
-    });
-
-    constructor(
-      private readonly maptoolGateway: MaptoolGateway,
-      @Optional() @Inject(TOKEN_SERVICE_TOKEN) private tokenService: ITokenService
-    ) {}
-
-    async ngOnInit(): Promise<void> {
-      if (!this.tokenService) {
-        console.warn('Token service is not available, skipping Maptool Gateway connection');
-      }
-     }
-
-    private async connectToMaptoolGateway(): Promise<void> {
-      const token = this.tokenService?.accessToken() || '';
-      if (token) {
-        await this.maptoolGateway.connect(token);
-      } else {
-        console.warn('No token found, unable to connect to Maptool Gateway');
-      }
+  autoGatewayConnection = effect(async () => {
+    if (this.tokenService && !this.maptoolGateway.isConnected()) {
+      await this.connectToMaptoolGateway();
     }
+  });
+
+  constructor(
+    private readonly maptoolGateway: MaptoolGateway,
+    @Optional() @Inject(TOKEN_SERVICE_TOKEN) private tokenService: ITokenService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    if (!this.tokenService) {
+      console.warn('Token service is not available, skipping Maptool Gateway connection');
+    }
+  }
+
+  private async connectToMaptoolGateway(): Promise<void> {
+    const token = this.tokenService?.accessToken() || '';
+    if (token) {
+      try {
+        await this.maptoolGateway.connect(token);
+      } catch (error) {
+        console.error('Error connecting to Maptool Gateway:', error);
+        setTimeout(async () => {
+          await this.connectToMaptoolGateway();
+        }, 5000);
+      }
+    } else {
+      console.warn('No token found, unable to connect to Maptool Gateway');
+    }
+  }
 }
--- apps/frontend/src/app/infrastructure/maptool.gateway.ts
+++ apps/frontend/src/app/infrastructure/maptool.gateway.ts
@@ -4,91 +4,99 @@ import { Subject } from "rxjs";
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { MaptoolMessage, Request, Response } from "@phobos-maptool/protocol";
 
-import { environment } from "../../environments/environment";
+const MAPTOOL_SERVER_HOSTNAME = window?.__env?.MAPTOOL_SERVER_HOSTNAME || window.location.hostname;
+const MAPTOOL_SERVER_PORT = window?.__env?.MAPTOOL_SERVER_PORT || 3002;
 
-const MAPTOOL_SERVER_HOSTNAME = environment.maptoolApiHostname || window?.__env?.trxServerHostname || window.location.hostname;
-const MAPTOOL_SERVER_PORT = environment.maptoolApiPort || window?.__env?.trxServerPort || 3002;
-
-const REST_API_URL = `http://${window.location.host}`;
-const WS_URL = `ws://${MAPTOOL_SERVER_HOSTNAME}:${MAPTOOL_SERVER_PORT}`;
+const WS_PROTOCOL = window.location.protocol === 'https:' ? 'wss' : 'ws';
+const WS_URL = `${WS_PROTOCOL}://${MAPTOOL_SERVER_HOSTNAME}:${MAPTOOL_SERVER_PORT}`;
 
 @Injectable(
-    { providedIn: 'root' }
+  { providedIn: 'root' }
 )
 export class MaptoolGateway {
-    public onRequest: Subject<{id: string, request: Request}> = new Subject<{id: string, request: Request}>();
-    public onMessage: Subject<MaptoolMessage> = new Subject<MaptoolMessage>();
-    public onOpen: Subject<void> = new Subject<void>();
-    public onClose: Subject<void> = new Subject<void>();
+  public onRequest: Subject<{ id: string, request: Request }> = new Subject<{ id: string, request: Request }>();
+  public onMessage: Subject<MaptoolMessage> = new Subject<MaptoolMessage>();
+  public onOpen: Subject<void> = new Subject<void>();
+  public onClose: Subject<void> = new Subject<void>();
 
-    public isConnected: WritableSignal<boolean> = signal(false);
+  public isConnected: WritableSignal<boolean> = signal(false);
 
-    private requests: Map<string, (value: Response) => void> = new Map<string, (value: Response) => void>();
-    private ws!: WebSocketSubject<any>;
+  private requests: Map<string, (value: Response) => void> = new Map<string, (value: Response) => void>();
+  private ws!: WebSocketSubject<any>;
 
-    constructor() {}
+  constructor() { }
 
-    public async connect(jwt: string) {
-        this.ws = webSocket({url: `${WS_URL}?token=${jwt}`, openObserver: { 
-            next: () => { 
-                this.isConnected.set(true); 
-                this.onOpen.next();
-        }}});
-
-        this.ws.subscribe({
-            next: this.handleMessage.bind(this),
-            error: this.handleClose.bind(this),
-            complete: this.handleClose.bind(this)
-        });
-    }
-
-    public request(req: Request): Promise<Response> {
-        return new Promise((resolve, reject) => {
-            const msg: MaptoolMessage = {
-                id: uuidv4(),
-                request: req
-            }
-            this.requests.set(msg.id, resolve.bind(this));
-            setTimeout(this.rejectOnTimeout.bind(this, msg.id, reject.bind(this, `${req} timed out`)), 5000);
-            this.ws.next({event: 'msg', data: JSON.stringify(MaptoolMessage.toJSON(msg))});
-        });
-
-    }
-
-    public respond(id: string, res: Response) {
-        const msg: MaptoolMessage = {
-            id: id,
-            response: res
+  public connect(jwt: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.ws = webSocket({
+        url: `${WS_URL}?token=${jwt}`,
+        openObserver: {
+          next: () => {
+            this.isConnected.set(true);
+            this.onOpen.next();
+            resolve();
+          }
         }
-        this.ws.next({event: 'msg', data: JSON.stringify(MaptoolMessage.toJSON(msg))});
+      });
+
+      this.ws.subscribe({
+        next: this.handleMessage.bind(this),
+        error: (err) => {
+          this.handleClose();
+          reject(err);
+        },
+        complete: this.handleClose.bind(this)
+      });
+    });
+  }
+
+  public request(req: Request): Promise<Response> {
+    return new Promise((resolve, reject) => {
+      const msg: MaptoolMessage = {
+        id: uuidv4(),
+        request: req
+      }
+      this.requests.set(msg.id, resolve.bind(this));
+      setTimeout(this.rejectOnTimeout.bind(this, msg.id, reject.bind(this, `${req} timed out`)), 5000);
+      this.ws.next({ event: 'msg', data: JSON.stringify(MaptoolMessage.toJSON(msg)) });
+    });
+
+  }
+
+  public respond(id: string, res: Response) {
+    const msg: MaptoolMessage = {
+      id: id,
+      response: res
+    }
+    this.ws.next({ event: 'msg', data: JSON.stringify(MaptoolMessage.toJSON(msg)) });
+  }
+
+  private handleMessage(buffer: { event: 'msg', data: string }) {
+    const msg = MaptoolMessage.fromJSON(JSON.parse(buffer.data));
+    console.log('MaptoolGateway: Received message', msg);
+    if (msg.request) {
+      this.onRequest.next({ id: msg.id, request: msg.request });
     }
 
-    private handleMessage(buffer: {event: 'msg', data: string}) {
-        const msg = MaptoolMessage.fromJSON(JSON.parse(buffer.data));
-        console.log('MaptoolGateway: Received message', msg);
-        if(msg.request) {
-            this.onRequest.next({id: msg.id, request: msg.request});
-        }
-
-        if(msg.response) {
-            if(this.requests.has(msg.id)) {
-                this.requests.get(msg.id)!(msg.response);
-                this.requests.delete(msg.id);
-            }
-        }
-
-        this.onMessage.next(msg);
+    if (msg.response) {
+      if (this.requests.has(msg.id)) {
+        this.requests.get(msg.id)!(msg.response);
+        this.requests.delete(msg.id);
+      }
     }
 
-    private handleClose() {
-        this.isConnected.set(false);
-        // setTimeout(this.connect.bind(this), 5000);
-        this.onClose.next();
-    }
+    this.onMessage.next(msg);
+  }
 
-    private rejectOnTimeout(id: string, reject: (reason?: any) => void) {
-        if(this.requests.delete(id)) {
-            reject();
-        };
-    }
+  private handleClose() {
+    this.isConnected.set(false);
+    // setTimeout(this.connect.bind(this), 5000);
+    this.onClose.next();
+  }
+
+  private rejectOnTimeout(id: string, reject: (reason?: any) => void) {
+    if (this.requests.delete(id)) {
+      reject();
+    };
+  }
 }
